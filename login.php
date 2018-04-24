f928fb4ab51c51e33e063fda3c3cc5bd7cff3852
0903934 <36078311+0903934@users.noreply.github.com>
2018-04-24 19:16:26 +0100

    Update login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if(empty($_POST["username"]) || empty($_POST ["password"]))
     $password=$_POST['password'];
 
     $sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
-    $result=mysqli_query($conn,$sql);
+    $result=mysqli_query($conn,$query);
 
     if (mysqli_num_rows($result) == 1)
     {
